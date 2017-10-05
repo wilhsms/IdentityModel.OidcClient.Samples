81a3681fd342cde4a44d850ee8e2765cc1ae1c43
Dominick Baier <dbaier@leastprivilege.com>
2017-10-05 13:55:37 +0200

    added iOS 11 example

---
+++ iOS11Client/iOS11Client/ViewController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using IdentityModel.OidcClient;
+using UIKit;
+
+namespace iOS11Client
+{
+    public partial class ViewController : UIViewController
+    {
+        OidcClient _client;
+        AuthorizeState _state;
+        SafariServices.SFAuthenticationSession _session;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+			var options = new OidcClientOptions
+			{
+				Authority = "https://demo.identityserver.io",
+				ClientId = "native.hybrid",
+				Scope = "openid profile email api",
+                RedirectUri = "SFAuthenticationSessionExample://callback",
+
+				ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect
+			};
+
+			_client = new OidcClient(options);   
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            LoginButton.TouchUpInside += LoginButton_TouchUpInside;
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        async void LoginButton_TouchUpInside(object sender, EventArgs e)
+        {
+			_state = await _client.PrepareLoginAsync();
+
+			_session = new SafariServices.SFAuthenticationSession(
+				new Foundation.NSUrl(_state.StartUrl),
+				"SFAuthenticationSessionExample://",
+				async (callbackUrl, error) =>
+				{
+                    if (error != null) 
+                    {
+                        OutputText.Text = error.ToString();
+                        return;
+                    }
+
+					var result = await _client.ProcessResponseAsync(callbackUrl.AbsoluteString, _state);
+                    if (result.IsError)
+                    {
+                        OutputText.Text = result.Error;
+                    }
+
+					var sb = new StringBuilder(128);
+					foreach (var claim in result.User.Claims)
+					{
+						sb.AppendFormat("{0}: {1}\n", claim.Type, claim.Value);
+					}
+
+					sb.AppendFormat("\n{0}: {1}\n", "refresh token", result?.RefreshToken ?? "none");
+					sb.AppendFormat("\n{0}: {1}\n", "access token", result.AccessToken);
+
+					OutputText.Text = sb.ToString();
+				});
+
+            _session.Start();
+		}
+    }
+}
---
+++ iOS11Client/iOS11Client/ViewController.designer.cs
@@ -0,0 +1,37 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace iOS11Client
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView OutputText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LoginButton != null) {
+                LoginButton.Dispose ();
+                LoginButton = null;
+            }
+
+            if (OutputText != null) {
+                OutputText.Dispose ();
+                OutputText = null;
+            }
+        }
+    }
+}
